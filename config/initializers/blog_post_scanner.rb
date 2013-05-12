694ea412fbfb343319e8fd36a898e5d7613e1ac3
tstone <wastingtape@gmail.com>
2013-05-11 21:32:18 -0600

    Added specs for posts controller

--- config/initializers/blog_post_scanner.rb
+++ config/initializers/blog_post_scanner.rb
@@ -1,16 +1,13 @@
 
-unless Rails.env.test?
-  require Rails.root.join("config/initializers/markdown")
+require Rails.root.join("config/initializers/markdown")
 
-  if Rails.env.development?
-    path = Rails.root.join("spec/fixtures")
-  else
-    path = Rails.root.join("posts")
-  end
-
-  puts "Scanning blog posts in #{path}..."
-
-  scanner = BlogPostScanner.new(path)
-  scanner.scan_and_store
-
+if Rails.env.development? or Rails.env.test?
+  path = Rails.root.join("spec/fixtures")
+else
+  path = Rails.root.join("posts")
 end
+
+puts "Scanning blog posts in #{path}..."
+
+scanner = BlogPostScanner.new(path)
+scanner.scan_and_store
--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -1,5 +1,18 @@
 require "spec_helper"
 
 describe PostsController do
+  describe "index" do
+    it "should get all the blog posts" do
+      get :index
+      assigns(:posts).should be_present
+    end
+  end
 
+  describe "show" do
+    it "should get all the blog posts" do
+      BlogPost.stub(:find).and_return(OpenStruct.new(title: "Example", slug: "example"))
+      get :show, id: "example"
+      assigns(:post).should be_present
+    end
+  end
 end
