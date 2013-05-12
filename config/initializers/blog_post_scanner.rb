aa44fa111ec0b2370db53939ee70e674f94bc372
tstone <wastingtape@gmail.com>
2013-05-11 22:42:31 -0600

    settings.yml/posts now respected

--- config/initializers/blog_post_scanner.rb
+++ config/initializers/blog_post_scanner.rb
@@ -5,7 +5,9 @@ require Rails.root.join("config/initializers/markdown")
 if Rails.env.development? or Rails.env.test?
   path = Rails.root.join("spec/fixtures")
 else
-  path = Rails.root.join("posts")
+  user_path = Rails.application.config.settings.posts
+  user_path = user_path[1, user_path.length] if user_path.start_with?("/")
+  path = Rails.root.join(user_path)
 end
 
 puts "Scanning blog posts in #{path}..."
