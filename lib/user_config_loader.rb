5b770b0dc9b4367f497be20516589e290cabd14c
tstone <wastingtape@gmail.com>
2013-05-19 22:46:45 -0600

    Refactored user config initializer to it's own class

---
+++ lib/user_config_loader.rb
@@ -0,0 +1,73 @@
+
+class UserConfigLoader
+
+  class << self
+
+    def asset_paths
+      %w( javascripts stylesheets images fonts )
+    end
+
+    def default_settings
+      {
+        host: "http://localhost:3000",
+        posts: "/posts",
+        count: 12,
+        title: "MicrowaveJS Blog",
+        desc:  "An example MicrowaveJS blog that you'll want to read again and again!",
+        author: "Blog Author",
+        forcehost: true,
+        posttimeformat: "MMMM d, yyyy",
+        comments: true,
+        disqusname: "testblog",
+        sharing: true,
+        twittersharing: true,
+        twittername:  "twittername",
+        facebooksharing: false,
+        googlesharing: false,
+        hackernewssharing: true,
+        theme: "default"
+      }
+    end
+
+    def load
+      raw_config = read_settings_yml
+      raw_config.reverse_merge!(default_settings)
+      Rails.application.config.settings = OpenStruct.new(raw_config.to_h)
+      rewrite_view_path
+      rewrite_asset_paths
+    end
+
+    def theme
+      Rails.application.config.settings.theme
+    end
+
+    def read_settings_yml
+      config_path = Rails.root.join("config/settings.yml")
+      raw_yml = File.read(config_path)
+      YAML.load(raw_yml)
+    end
+
+    def rewrite_view_path
+      ActionController::Base.class_eval do
+        paths = [
+          "themes/#{Rails.application.config.settings.theme}/views",
+          "themes/default/views"
+        ].select { |p| Dir.exists?(Rails.root.join(p)) }
+        self.view_paths = ActionView::PathSet.new(paths.uniq)
+      end
+    end
+
+    def rewrite_asset_paths
+      Rails.application.config.assets.paths = []
+      asset_paths.each { |p| add_asset_path(p, theme) } if theme
+      asset_paths.each { |p| add_asset_path(p, "default") } if !theme or theme != "default"
+    end
+
+    def add_asset_path(p, theme)
+      path = Rails.root.join("themes", theme, "assets", p)
+      if Dir.exists?(path)
+        Rails.application.config.assets.paths << path
+      end
+    end
+  end
+end
